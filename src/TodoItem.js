235a7b694c221802b6b990380107d9627034d6eb
Katharine Cooney <katharinecooney@gmail.com>
2019-07-02 15:34:31 -0500

    Add logic to TodoList to display all todos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 class TodoItem extends Component {
   render(){
     return(
-      <h1>TODO ITEM</h1>
+      <div>
+        <p>{this.props.task}</p>
+        <span>Edit</span>
+        <span>Delete</span>
+      </div>
     )
   }
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 import NewTodoForm from './NewTodoForm';
+import uuid from 'uuid';
 
 class TodoList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      allTodos: [{task: 'Apply to jobs', id: uuid()},
+      {task: 'Read', id: uuid()}]
+    }
+  }
+  
   render(){
+    let todos = this.state.allTodos.map(todo => <TodoItem task={todo.task} id={todo.id} key={todo.id}/>)
     return(
       <div>
         <h1>TODO LIST</h1>
-        <TodoItem />
+        {todos}
         <NewTodoForm />
       </div>
     )
